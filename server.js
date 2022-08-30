fc16fd9a2ff8f1996af8634e4b1097905b20c0f8
JordanTownshend <91408554+JordanTownshend@users.noreply.github.com>
2022-08-31 09:56:10 +1200

    Heroku implementation

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ const port = process.env.PORT || 8082;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "client/build")))
+    app.use(express.static(path.join(__dirname, "frontend/build")))
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build"), "index.html")
+        res.sendFile(path.join(__dirname, "frontend", "build"), "index.html")
     })
 } else {
     app.get("/", (req, res) => {res.send("API is running")})
