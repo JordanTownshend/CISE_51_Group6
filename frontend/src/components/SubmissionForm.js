b2a1fc306769f32c0b07ce8e24076527b784fa67
JordanTownshend <91408554+JordanTownshend@users.noreply.github.com>
2022-10-05 09:17:10 +1300

    Test now functions and passes. Feature complete

--- frontend/src/components/SubmissionForm.js
+++ frontend/src/components/SubmissionForm.js
@@ -9,14 +9,14 @@ const SubmissionForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
     
-      <input {...register("author", {required: true})} placeholder="Author" />
-      <p><input {...register("title", {required: true})} placeholder="Title" /></p>
-      <p><input {...register("journal", {required: true})} placeholder="Journal" /></p> 
-      <p><input {...register("pubyear", {required: true})} placeholder="Publication Year" /></p>
-      <p><input {...register("volume")} placeholder="Volume No." /></p>
-      <p><input {...register("doi")} placeholder="DOI" /></p>
-      <p><input {...register("issn")} placeholder="ISSN" /></p>
-      <p><input {...register("other")} placeholder="Other" /></p>
+      <input {...register("author", {required: true})} placeholder="Author" data-testid = "author"/>
+      <p><input {...register("title", {required: true})} placeholder="Title" data-testid = "title"/></p>
+      <p><input {...register("journal", {required: true})} placeholder="Journal" data-testid = "journal"/></p> 
+      <p><input {...register("pubyear", {required: true})} placeholder="Publication Year" data-testid = "pubyear"/></p>
+      <p><input {...register("volume")} placeholder="Volume No." data-testid = "volume"/></p>
+      <p><input {...register("doi")} placeholder="DOI" data-testid = "doi"/></p>
+      <p><input {...register("issn")} placeholder="ISSN" data-testid = "issn"/></p>
+      <p><input {...register("other")} placeholder="Other" data-testid = "other"/></p>
 
       <p>{errors.author && <span>Author field is required</span>}</p>
       <p>{errors.title && <span>Title field is required</span>}</p>
--- frontend/src/components/__tests__/SubmissionForm.test.js
+++ frontend/src/components/__tests__/SubmissionForm.test.js
@@ -4,9 +4,18 @@ import "@testing-library/jest-dom";
 
 import SubmissionForm from "../SubmissionForm";
 
+// tests that all relevant fields are displayed
 describe("SubmissionForm", () => {
     it("should render the basic fields", () => {
       render(<SubmissionForm />);
-      expect(screen.getByRole("textbox", {name: "author"})).toBeInTheDocument();
+
+      expect(screen.getByTestId("author")).toBeInTheDocument();
+      expect(screen.getByTestId("title")).toBeInTheDocument();
+      expect(screen.getByTestId("journal")).toBeInTheDocument();
+      expect(screen.getByTestId("pubyear")).toBeInTheDocument();
+      expect(screen.getByTestId("volume")).toBeInTheDocument();
+      expect(screen.getByTestId("doi")).toBeInTheDocument();
+      expect(screen.getByTestId("issn")).toBeInTheDocument();
+      expect(screen.getByTestId("other")).toBeInTheDocument();
     });
   });
