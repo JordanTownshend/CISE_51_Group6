7f8495f83b728dedcb0fd471415f72a8bda43b8e
ASR498 <75335998+ASR498@users.noreply.github.com>
2022-10-08 22:45:47 +1300

    frontend showing up now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 
 import Home from "./pages/Home";
-import SEPractice from "./pages/SE-Practice";
+
 import SubmitArticle from "./pages/Submit-Article"; 
 import NotFoundPage from "./pages/404";
 import Moderation from "./pages/Moderation";
@@ -19,19 +19,19 @@ const App = () =>  {
           <h1>Software Engineering Empirical Evidence Database (SEED)</h1>
             <ul className="header">
                 <li><NavLink exact to = "/">Home</NavLink></li>
-                <li><NavLink to = "/SEPractice">Select the Practice</NavLink></li>
+                
                 <li><NavLink to = "/SubmitArticle">Submit an Article</NavLink></li>
                 <li><NavLink to ="/Moderation">Moderation</NavLink></li>
             </ul>
           <div className="content">
-            <Routes>
+            
             <Route exact path="/" component={Home}/>
-            <Route  path="/SEPractice" component={SEPractice}/>
+            
             <Route  path="/SubmitArticle" component={SubmitArticle}/>
             <Route  path="/Moderation" component={Moderation}/>
             <Route  path="/404" component={NotFoundPage}/>
             <Redirect to="/404" />
-            </Routes>
+            
 
           </div>
         </div>
