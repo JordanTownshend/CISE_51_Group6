3d502062852f4bd7606ce383b9f213609b2868fd
ASR498 <75335998+ASR498@users.noreply.github.com>
2022-10-13 00:58:56 +1300

    login page with single admin user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React , { useState } from "react";
 import {
   Route,
   NavLink,
@@ -13,11 +13,93 @@ import AnalyseArticle from "./pages/Analyse-Article";
 
 import Search from "./components/Seacrh"
 
+
+
+
+
 const App = () =>  {
-    return (
+
+
+ // React States
+ const [errorMessages, setErrorMessages] = useState({});
+ const [isSubmitted, setIsSubmitted] = useState(false);
+
+ // User Login info
+ const database = [
+   {
+     username: "user1",
+     password: "pass1"
+   },
+   {
+     username: "user2",
+     password: "pass2"
+   }
+ ];
+
+ const errors = {
+   uname: "invalid username",
+   pass: "invalid password"
+ };
+
+ const handleSubmit = (event) => {
+   //Prevent page reload
+   event.preventDefault();
+
+   var { uname, pass } = document.forms[0];
+
+   // Find user login info
+   const userData = database.find((user) => user.username === uname.value);
+
+   // Compare user info
+   if (userData) {
+     if (userData.password !== pass.value) {
+       // Invalid password
+       setErrorMessages({ name: "pass", message: errors.pass });
+     } else {
+       setIsSubmitted(true);
+     }
+   } else {
+     // Username not found
+     setErrorMessages({ name: "uname", message: errors.uname });
+   }
+ };
+
+ // Generate JSX code for error message
+ const renderErrorMessage = (name) =>
+   name === errorMessages.name && (
+     <div className="error">{errorMessages.message}</div>
+   );
+
+ // JSX code for login form
+ const renderForm = (
+   <div className="form">
+     <form onSubmit={handleSubmit}>
+       <div className="input-container">
+         <label>Username </label>
+         <input type="text" name="uname" required />
+         {renderErrorMessage("uname")}
+       </div>
+       <div className="input-container">
+         <label>Password </label>
+         <input type="password" name="pass" required />
+         {renderErrorMessage("pass")}
+       </div>
+       <div className="button-container">
+         <input type="submit" />
+       </div>
+     </form>
+   </div>
+ );
+
+ return (
+   <div className="app">
+     <div className="login-form">
+       <div className="title"></div>
+       {isSubmitted ? <div>    
         <Router>
         <div>
           <h1>Software Practice Empirical Evidence Database (SPEED)</h1>
+          <h3> Administrator logged in</h3>
           <h3> Search Articles by Title</h3>
         <Search></Search>
             <ul className="header">
@@ -34,7 +116,16 @@ const App = () =>  {
           </div>
         </div>
         </Router>
-    );
+      </div> : renderForm}
+     </div>
+   </div>
+ );
+
+
+
+
+  
+
 }
  
 export default App;
