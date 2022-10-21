931314f733a5c8bb6ba5f3aeebe827cacb2b64e5
JordanTownshend <91408554+JordanTownshend@users.noreply.github.com>
2022-10-21 20:42:32 +1300

    Revert "app test added"
    This reverts commit bb25047dc7f55d672a2685998a4cf8b0fd9229b7.

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const connectDB = require('../config/db');
+var cors = require('cors');
+const server = express();
+const articles = require('../routes/api/articles');
+const dotenv = require("dotenv")
+
+connectDB();
+
+server.use(cors({ origin: true, credentials: true }));
+server.use(express.json({ extended: false }));
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+server.use('/api/articles', articles);
+
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+server.use(express.static(path.resolve(__dirname, "./client/build")));
+
+// Step 2:
+server.get("*", function (request, response) {
+
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+
+});
+
+server.listen(PORT, console.log(`server is working and listening on PORT ${PORT}`));
